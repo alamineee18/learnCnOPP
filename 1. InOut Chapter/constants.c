fd977e0858f243bd6cebc11913b26dbb22d19c55
Md. Al-Amin Kabir <109284049+alamineee18@users.noreply.github.com>
2024-06-12 18:06:49 +0600

    constants

---
+++ 1. InOut Chapter/constants.c
@@ -0,0 +1,28 @@
+//Contant: Values that don't change (fixed)
+
+/*
+Types
+1. integer constant 1, 2, 3, 0, -1, -2
+2. Real Constants 1.0, 2.2, 3.134, -0.234
+3. Character Constants 'a', 'A', '#'
+*/
+
+// Keywords: Reserved words that have special meaning to the compiler
+// c have 32 keywords
+/*
+auto      double  int         struct
+break     else    long        switch
+case      enum    register    typedef
+char      extern  return      union
+continue  for     signed      void
+do        if      static      while
+default   goto    sizeof      volatile
+const     float   short       unsigned
+*/
+
+
+#include <stdio.h>
+int main()
+{
+
+}
