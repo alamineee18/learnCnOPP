06300cb2937d06638bf1f2291a8a661a17c8786a
Md. Al-Amin Kabir <alaminkabir789@gmail.com>
2024-07-25 13:42:30 +0600

    Update inheritance.cpp

---
+++ OOP/inheritance.cpp
@@ -0,0 +1,24 @@
+//When properties & member function of base class are passws on the derived class
+ 
+ #include<iostream>
+ using namespace std;
+
+ class Person {
+public:
+    string name;
+    int age;
+
+    Person(string name, int age){
+        this->name = name;
+        this->age = age;
+    }
+ };
+
+ class Student : public Person
+
+ int main()
+ {
+
+
+    return 0;
+ }
