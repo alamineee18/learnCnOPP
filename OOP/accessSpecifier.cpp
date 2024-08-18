0fb28c8897cbf10707ed2e407905f22001a9ca8b
Md. Al-Amin Kabir <alaminkabir789@gmail.com>
2024-08-18 18:04:08 +0600

    Update accessSpecifier.cpp

---
+++ OOP/accessSpecifier.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class areaofHouse
+{
+private: 
+    int length;
+    int base;
+// public:
+    void setData (length, base){
+        length;
+        base;
+    }
+    
+
+};
+
+int main()
+{
+    // areaofHouse gini;
+
+
+
+    return 0;
+}
