c0db1a67be761b7025626a420d99ee9e1897d342
Md. Al-Amin Kabir <alaminkabir789@gmail.com>
2024-08-18 18:30:13 +0600

    update

--- OOP/accessSpecifier.cpp
+++ OOP/accessSpecifier.cpp
@@ -6,20 +6,27 @@ class areaofHouse
 private: 
     int length;
     int base;
-// public:
-    void setData (length, base){
-        length;
-        base;
+public:
+    void setData (int x, int y){
+        length = x;
+        base = y;
     }
-    
+
+    void Area (){
+        cout << "The area of House: " << length * base << endl;
+    }
 
 };
 
 int main()
 {
-    // areaofHouse gini;
+    areaofHouse house_1, house_2;
+    house_1.setData(200, 100);
+    house_1.Area();
 
-
+    // area of second house 
+    house_2.setData(10, 5);
+    house_2.Area();
 
     return 0;
 }
--- OOP/inheritance.cpp
+++ OOP/inheritance.cpp
@@ -14,8 +14,6 @@ public:
     }
  };
 
- class Student : public Person
-
  int main()
  {
 
